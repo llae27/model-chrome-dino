cb1f15b26ddad62196cab5d301f1a9d9595d66b8
llae27 <youllae@snu.ac.kr>
2026-02-22 23:38:13 -0800

    Build and fix default features

---
+++ README.md
@@ -0,0 +1,23 @@
+# model-chrome-dino
+
+## Installation
+
+The chrome driver is set to match chrome major version 145.
+
+### Linux chrome
+
+If you're working on linux, use the following command to download full chrome.
+
+`wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb`
+
+## Gymnasium usage
+
+The original documentation includes few bugs. I've listed fixes below.
+
+To turn on the default acceleration of the game, add the following line.
+
+`env.unwrapped.set_acceleration(True)`
+
+To set a custom value for the acceleration, use the following line.
+
+`env.unwrapped.game.set_parameter("config.ACCELERATION", [VALUE])`
--- bin/worker.py
+++ bin/worker.py
@@ -1,11 +1,27 @@
 import gymnasium as gym
 import gym_chrome_dino
+from gym_chrome_dino.utils.wrappers import make_dino
+from gym_chrome_dino.envs.chrome_dino_env import ACTION_MEANING
 
 
 def main():
+    # initialize env
     env = gym.make("ChromeDinoNoBrowser-v0")
-    # env = gym.make("ChromeDino-v0")
-    print(env)
+    env = make_dino(env, timer=True, frame_stack=True)
+    env.unwrapped.set_acceleration(True)
+    # env.unwrapped.game.set_parameter("config.ACCELERATION", 1)  # default value 0.001
+    params = env.unwrapped.game.get_parameters()
+    print(f"Current acceleration: {params['config.ACCELERATION']}")
+    env.reset()
+    # run experiment
+    terminated = False
+    while not terminated:
+        action = env.action_space.sample()
+        observation, reward, terminated, truncated, info = env.step(action)
+        print(f"{ACTION_MEANING[action]} {reward} / {info}")
+    # report
+    score = env.unwrapped.get_score()
+    print(f"Final score: {score}")
 
 
 if __name__ == "__main__":
